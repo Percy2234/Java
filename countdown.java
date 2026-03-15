cd8fbe15d0e3a8599ec1d27f3e9f4369fc459a9d
Percy2234 <hyun9henry@gmail.com>
2026-03-15 22:37:35 +1100

    Add files via upload

--- countdown.java
+++ countdown.java
@@ -1,5 +1,14 @@
 public class countdown {
     public static void main(String[] args) {
+        System.out.println("Enter a positive integer: ");
+        int n = In.nextInt();
+        for (int i = n; i > 0; i--) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is Even");
+            } else {
+                System.out.println(i + " is ODD");
+            }
 
+        }
     }
 }
